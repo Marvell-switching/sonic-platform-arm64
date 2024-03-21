7118035f0562bc965fa734facabe81b9c0b44be1
Pavan Naregundi <pnaregundi@marvell.com>
2024-03-21 10:07:24 +0000

    Falcon12.8/6.4/2T platform support
    Signed-off-by: Pavan Naregundi <pnaregundi@marvell.com>

---
+++ db98cx8540-16cd/scripts/db98cx8540-16cd-init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Platform init script for db98cx8540-16cd 
+db98cx8540-16cd_profile()
+{
+    MAC_ADDR=$(ip link show eth0 | grep ether | awk '{print $2}')
+    find /usr/share/sonic/device/*db98cx8540_16cd* -name profile.ini | xargs sed -i "s/switchMacAddress=.*/switchMacAddress=$MAC_ADDR/g"
+    echo "db98cx8540-16cd: Updating switch mac address ${MAC_ADDR}"
+}
+
+# - Main entry
+db98cx8540-16cd_profile
+
+# - Main entry
+# LOGIC to enumerate SFP eeprom devices - send 0x50 to kernel i2c driver - initialize devices
+echo optoe2 0x50 > /sys/bus/i2c/devices/i2c-2/new_device
+
+exit 0
--- db98cx8580-32cd/scripts/db98cx8580-32cd-init.sh
+++ db98cx8580-32cd/scripts/db98cx8580-32cd-init.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 # Platform init script for db98cx8580-32cd 
-
+db98cx8580-32cd_profile()
+{
+    MAC_ADDR=$(ip link show eth0 | grep ether | awk '{print $2}')
+    find /usr/share/sonic/device/*db98cx8580_32cd* -name profile.ini | xargs sed -i "s/switchMacAddress=.*/switchMacAddress=$MAC_ADDR/g"
+    echo "db98cx8580-32cd: Updating switch mac address ${MAC_ADDR}"
+}
 
 # - Main entry
+db98cx8580-32cd_profile
+
 # LOGIC to enumerate SFP eeprom devices - send 0x50 to kernel i2c driver - initialize devices
 echo optoe2 0x50 > /sys/bus/i2c/devices/i2c-2/new_device
 
